03a92eceaa8cb0557653cbc47baa519d62bb5fa9
AlexKennedy <alexander-kennedy@uiowa.edu>
2016-03-10 17:56:09 -0600

    finished tables
    top 10 tables for spam ham and same but for more than 3 characters

--- untitledq1.py
+++ untitledq1.py
@@ -4,6 +4,8 @@ def analyzeSMSes(inputFilename):
     spamWordCounts = {}
     hamWordCounts = {}
     SMSTextList = getSMSList(inputFilename)
+    hamTextList = []
+    spamTextList = []
 
     for index in range(len(SMSTextList)):
         SMSTextList[index] = SMSTextList[index].split()
@@ -17,38 +19,68 @@ def analyzeSMSes(inputFilename):
             for word in message[1:]:
                 if word != '':
                     spamWordCounts[word] = spamWordCounts.get(word,0) + 1
-                    
-    #print top 10 frequent spam words
-    spamTuples = list(zip(spamWordCounts.keys(), spamWordCounts.values()))
-    sortedSpamTuples = sorted(spamTuples, key = lambda item: item[1],reverse = True)
-    print("The 10 most common words in the spam messages (any number of characters) are:")
-    for wordNumber in range(10):
-        print((wordNumber + 1),".)",sep = '', end = ' ')
-        print(sortedSpamTuples[wordNumber][0]," - ",sortedSpamTuples[wordNumber][1],sep='')
 
-    print()
+    #separate ham and spam texts into different lists
+    for message in SMSTextList:
+        if message[0] == 'spam':
+            spamTextList.append(message)
+        else:
+            hamTextList.append(message)
+    
     totalSpamWords = sum(spamWordCounts.values())
     totalHamWords = sum(hamWordCounts.values())
-    #print top 10 frequent ham words
-    hamTuples = list(zip(hamWordCounts.keys(), hamWordCounts.values()))
-    frequencies = [frequency for  in hamTuples]
-    hamTuplesForTable = []
-    sortedHamTuples = sorted(hamTuples, key = lambda item: item[1],reverse = True)
-    print("The 10 most common words in the ham messages (any number of characters) are:")
-    for wordNumber in range(10):
-        ham = sortedHamTuples[wordNumber]
-        hamFrequency = '{n:.{d}f}'.format(n=(ham[1] / totalHamWords),d = 4)
-        print((wordNumber + 1),".)",sep = '', end = ' ')
-        print(ham[0]," - ",ham[1]," Word Frequency: ",hamFrequency,sep='')
+    #print top 10 frequent spam words
+    spamTuples = sorted(spamWordCounts.items(), key = lambda item: item[1],reverse = True)
+    hamTuples = sorted(hamWordCounts.items(), key = lambda item: item[1],reverse = True)
+    spamTableFormat = []
+    hamTableFormat = []
+    spamFrequency = []
+    hamFrequency = []
+    spamMessageFrequency = []
+    hamMessageFrequency = []
+    print("SPAM Words:\nWord: the word associated with the stats\nWord Count: amount of times that word occurs in total throughout the text messages inputted\nWord Frequency: the percentage of how often the word occurs\nMessage Frequency: the percentage of how many messages contain this word\n")
+    #populate spam frequency list in order of sorted list
+    for item in spamTuples:
+        spamFrequency.append('{n:.{d}f}'.format(n=(item[1]/totalSpamWords),d = 4))
+    #populate spam message frequency list in order of sorted list
+    for wordCount in spamTuples:
+        totalMessages = 0
+        totalMessagesContainingWord = 0
+        for line in spamTextList:
+            if wordCount[0] in line:
+                totalMessagesContainingWord += 1
+            totalMessages += 1
+        spamMessageFrequency.append('{n:.{d}f}'.format(n=(totalMessagesContainingWord/totalMessages),d = 4))
+    for index in range(len(spamTuples)):
+        spamTableFormat.append((spamTuples[index][0],spamTuples[index][1],spamFrequency[index],spamMessageFrequency[index]))
+    template = "| {0:6} | {1:10} | {2:14} | {3:17} |"
+    printTable(template,spamTableFormat)
 
-    #print the top 10 information about words in a pretty table format
-    template = "{0:6}|{1:10}|{2:14}"
-    print (template.format("WORD", "WORD COUNT","WORD FREQUENCY"))
-    print("================================")
-    for rec in sortedHamTuples[:10]: 
-        print (template.format(*rec))
-        print("--------------------------------")
-    #hamTuplesMoreThan3Letters = [item for item in hamTuples if len(item[0]) > 5]
+    print("\nHAM Words:\nWord: the word associated with the stats\nWord Count: amount of times that word occurs in total throughout the text messages inputted\nWord Frequency: the percentage of how often the word occurs\nMessage Frequency: the percentage of how many messages contain this word\n") 
+    #populate ham frequency list in order of sorted list
+    for item in hamTuples:
+        hamFrequency.append('{n:.{d}f}'.format(n=(item[1]/totalHamWords),d = 4))
+    #populate message ham frequency list in order of sorted list
+    for wordCount in hamTuples:
+        totalMessages = 0
+        totalMessagesContainingWord = 0
+        for line in hamTextList:
+            if wordCount[0] in line:
+                totalMessagesContainingWord += 1
+            totalMessages += 1
+        hamMessageFrequency.append('{n:.{d}f}'.format(n=(totalMessagesContainingWord/totalMessages),d = 4))
+    for index in range(len(spamTuples)):
+        hamTableFormat.append((hamTuples[index][0],hamTuples[index][1],hamFrequency[index],hamMessageFrequency[index]))
+    printTable(template,hamTableFormat)
+
+    print("\nHAM Words (containing more than 3 characters):\n")
+    hamTuplesMoreThan3Letters = [item for item in hamTableFormat if len(item[0]) > 3]
+    printTable(template,hamTuplesMoreThan3Letters)
+
+    print("\nSPAM Words (containing more than 3 characters):\n")
+    spamTuplesMoreThan3Letters = [item for item in spamTableFormat if len(item[0]) > 3]
+    printTable(template,spamTuplesMoreThan3Letters)
+
     #sortedTuplesGreater3 = sorted(hamTuplesMoreThan3Letters,key = lambda item: item[1], reverse = True)
     #print(sortedTuplesGreater3[:10])
     #print(hamTuples[:10])
@@ -57,6 +89,16 @@ def analyzeSMSes(inputFilename):
     #sortedHamWordCounts = sorted(hamWordCounts, 
     #print(sortedHamWordCounts,spamWordCounts)
     #print(SMSTextList[:10])
+
+#prints a table from the tuples passed in in table format
+def printTable(template,tuples):
+    print("------------------------------------------------------------")
+    print (template.format("WORD", "WORD COUNT","WORD FREQUENCY","MESSAGE FREQUENCY"))
+    print("============================================================")
+    for rec in tuples[:10]: 
+        print (template.format(*rec))
+        print("------------------------------------------------------------")
+
 
 #makes a list of the SMS messages, each member of the list is a message.
 def getSMSList(filename):
